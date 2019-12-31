fcb1354b6a4c2973cfb9f74de0dcf92c496b6cdc
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2019-12-31 12:09:08 +0100

    Refactor, not working yet

--- src/lib.rs
+++ src/lib.rs
@@ -6,84 +6,112 @@ use crossbeam_channel::Sender;
 use std::thread;
 use std::time::Duration;
 
-const NOT_IMPORTANT: usize = 0;
-
-pub struct Transition {
-    blinkers: Blinkers,
-    transition: Vec<Message>,
-    success_msg: Option<Message>,
-    failure_msg: Option<Message>,
+pub struct Transition<T: Task, M: Messg> {
+    task: T,
+    success_msg: M,
+    failure_msg: M,
 }
 
-impl From<&Vec<String>> for Transition {
-    #[must_use]
-    fn from(colors: &Vec<String>) -> Self {
-        let blinkers: Blinkers =
-            Blinkers::new().unwrap_or_else(|_| panic!("Could not find device"));
+impl<T: Task, M: Messg> Transition<T, M> {
+    pub fn new(task: T, success_msg: M, failure_msg: M) -> Self {
+        Transition {
+            task,
+            success_msg,
+            failure_msg,
+        }
+    }
+}
+
+impl<T: Task, M: Messg> Transition<T, M> {
+    pub fn start(self) -> Result<Transmitter, failure::Error> {
+        let (sender, receiver) = unbounded();
+        let success_msg = self.success_msg;
+        let failure_msg = self.failure_msg;
+        let task = self.task;
+        thread::spawn(move || -> Result<(), failure::Error> {
+            loop {
+                match receiver.recv() {
+                    Ok(Msg::Success) => break success_msg.send(),
+                    Ok(Msg::Failure) => break failure_msg.send(),
+                    Err(_) => {}
+                };
+                task.execute()?
+            }
+        });
+        Ok(Transmitter { sender })
+    }
+}
+
+pub trait Task: Send + 'static {
+    fn execute(&self) -> Result<(), failure::Error>;
+}
+
+pub struct BlinkTask {
+    transition: Vec<Message>,
+    blinkers: &'static Blinkers,
+}
+
+impl BlinkTask {
+    pub fn new(colors: &Vec<&str>, blinkers: &'static Blinkers) -> Self {
         let mut transition = Vec::new();
-        for color_name in colors {
+        for &color_name in colors {
             transition.push(Message::Fade(
-                Color::from(color_name.as_str()),
+                Color::from(color_name),
                 Duration::from_millis(500),
             ));
         }
         Self {
             blinkers,
             transition,
-            success_msg: None,
-            failure_msg: None,
         }
     }
 }
 
-impl Transition {
-    pub fn start(self) -> Result<Transmitter, failure::Error> {
-        let (sender, receiver) = unbounded();
-        let no_transitions = self.transition.len();
-        thread::spawn(move || -> Result<usize, failure::Error> {
-            loop {
-                match receiver.try_recv() {
-                    Ok(Msg::Success) => break self.send_success_msg(),
-                    Ok(Msg::Failure) => break self.send_failure_msg(),
-                    Err(_) => {}
-                };
-                self.play_transition();
-            }
-        });
-        let duration = Duration::from_millis(500 * no_transitions as u64 + 50);
-        Ok(Transmitter { sender, duration })
-    }
-
-    fn send_success_msg(&self) -> Result<usize, failure::Error> {
-        self.blinkers
-            .send(self.success_msg.unwrap_or_else(|| color_msg("green")))?;
-        Ok(NOT_IMPORTANT)
-    }
-
-    fn send_failure_msg(&self) -> Result<usize, failure::Error> {
-        println!("blinking with failure");
-        self.blinkers
-            .send(self.failure_msg.unwrap_or_else(|| color_msg("red")))?;
-        Ok(NOT_IMPORTANT)
-    }
-
-    fn play_transition(&self) {
+impl Task for BlinkTask {
+    fn execute(&self) -> Result<(), failure::Error> {
         for &message in &self.transition {
             self.blinkers.send(message).unwrap();
             std::thread::sleep(Duration::from_millis(500));
         }
+        Ok(())
     }
+}
 
-    #[must_use]
-    pub fn on_success(mut self, color_name: &str) -> Self {
-        self.success_msg = Some(color_msg(color_name));
-        self
+pub trait Messg: Send + 'static {
+    fn send(&self) -> Result<(), failure::Error>;
+}
+
+pub(crate) struct SuccessMsg {
+    blinkers: &'static Blinkers,
+}
+
+impl SuccessMsg {
+    fn new(blinkers: &'static Blinkers) -> Self {
+        Self { blinkers }
     }
+}
 
-    #[must_use]
-    pub fn on_failure(mut self, color_name: &str) -> Self {
-        self.failure_msg = Some(color_msg(color_name));
-        self
+impl Messg for SuccessMsg {
+    fn send(&self) -> Result<(), failure::Error> {
+        self.blinkers.send(color_msg("green"))?;
+        Ok(())
+    }
+}
+
+pub(crate) struct FailureMsg {
+    blinkers: &'static Blinkers,
+}
+
+impl FailureMsg {
+    fn new(blinkers: &'static Blinkers) -> Self {
+        Self { blinkers }
+    }
+}
+
+impl Messg for FailureMsg {
+    fn send(&self) -> Result<(), failure::Error> {
+        self.blinkers.send(color_msg("red"))?;
+        Ok(())
     }
 }
 
@@ -94,23 +122,34 @@ pub enum Msg {
 
 pub struct Transmitter {
     sender: Sender<Msg>,
-    duration: Duration,
 }
 
 impl Transmitter {
     pub fn notify_success(&self) -> Result<(), failure::Error> {
         self.sender.send(Msg::Success)?;
-        thread::sleep(self.duration);
         Ok(())
     }
 
     pub fn notify_failure(&self) -> Result<(), failure::Error> {
         self.sender.send(Msg::Failure)?;
-        thread::sleep(self.duration);
         Ok(())
     }
 }
 
 fn color_msg(color_name: &str) -> Message {
     Message::Fade(Color::from(color_name), Duration::from_millis(500))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() -> Result<(), failure::Error> {
+        let blinkers: Blinkers =
+            Blinkers::new().unwrap_or_else(|_| panic!("Could not find device"));
+        let blink_task = BlinkTask::new(&vec!["blue", "white"], &blinkers);
+        let failure_msg = FailureMsg::new(&blinkers);
+        Ok(())
+    }
 }
