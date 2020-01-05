cfed12612c77139e761bf08cd6f1eed056d38257
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-01-05 18:10:04 +0100

    Add intro doc for the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! This crate allows to programmatically control LED light - blink(1). The purpose is to be able
+//! to show the status of your task by "wrapping" all it's execution between calls to this library.
+//!
+//! While your code is executing, the LED transition between two specified colors indicating
+//! "pending" state. After your code finishes execution, you can notify the LED about the status
+//! of your task, changing it's light to one of two colors - depending on the status of your code.
+//!
+//! # Example
+//!
+//! ```rust
+//! use transition::Transition;
+//! use std::error::Error;
+//! use std::thread;
+//! use std::time::Duration;
+//!
+//! fn main() -> Result<(), Box<dyn Error>> {
+//!     let notifier = Transition::new(&["blue", "white"]) // pending state
+//!         .on_success("green")
+//!         .on_failure("red")
+//!         .start()?;
+//!
+//!     // your code here
+//!     thread::sleep(Duration::from_secs(2));
+//!
+//!     notifier.notify_success(); // or notifier.notify_failure();
+//!     // LED changes color to the one specified for success
+//!     Ok(())
+//! }
+//! ```
+
 #[cfg(test)]
 mod testutils;
 
