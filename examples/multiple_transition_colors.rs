f27433906e84c995418e06bb1f0c983cb00ea5c3
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-08-08 13:03:34 +0200

    Add example of multiple transition colors

---
+++ examples/multiple_transition_colors.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+use transition::{Led, Transition};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let notification = Transition::new(&[
+        Led::Cyan,
+        Led::Blank,
+        Led::Yellow,
+        Led::Blank,
+        Led::White,
+        Led::Blank,
+        Led::Orange,
+        Led::Blank,
+    ])
+    .start()?;
+
+    thread::sleep(Duration::from_secs(5));
+
+    notification.notify_success()?;
+
+    Ok(())
+}
