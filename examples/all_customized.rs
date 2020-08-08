5719ad2f6e03d58d78b2c7b1d82d32a59aef6343
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-08-08 13:08:42 +0200

    Add example of all customized

---
+++ examples/all_customized.rs
@@ -0,0 +1,15 @@
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+use transition::{Led, Transition};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let notification = Transition::new(&[Led::Cyan, Led::Blank, Led::Orange, Led::Blank])
+        .on_success(Led::Red)
+        .on_failure(Led::Green)
+        .start()?;
+    thread::sleep(Duration::from_secs(5));
+    notification.notify_success()?;
+
+    Ok(())
+}
