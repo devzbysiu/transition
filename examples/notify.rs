0e0b726744938efc48cf01a5c6c0ea2a3800f151
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-08-08 13:36:09 +0200

    Add separate example of notifications

--- examples/notify.rs
+++ examples/notify.rs
@@ -4,8 +4,17 @@ use std::time::Duration;
 use transition::Transition;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // transition finished with success
     let notification = Transition::default().start()?;
     thread::sleep(Duration::from_secs(5));
     notification.notify_success()?;
+
+    thread::sleep(Duration::from_secs(5));
+
+    // transition finished with failure
+    let notification = Transition::default().start()?;
+    thread::sleep(Duration::from_secs(5));
+    notification.notify_failure()?;
+
     Ok(())
 }
